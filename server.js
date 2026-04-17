daefb9dc01ce0dc1dd30ad1e208dbe3a668d43e5
sayansanpui <sayansanpuicoding@gmail.com>
2026-04-17 15:28:29 +0530

    Add CORS middleware and update package dependencies

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import "dotenv/config";
+import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
+app.use(cors({
+    origin: "*",
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
